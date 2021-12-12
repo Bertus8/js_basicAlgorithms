6444f31ed433261153581ec2f41379a6b1fcfdb2
Bertus8 <albertinisp@gmail.com>
2021-12-12 10:37:37 +0100

    function iteration1

---
+++ js_functions/iteration1.js
@@ -0,0 +1,12 @@
+
+function sum(numberOne , numberTwo) {
+    if (numberOne > numberTwo) {
+        console.log("numberOne");
+        return numberOne;
+    } else {     
+        console.log("numberTwo");
+        return numberTwo;
+    }
+  };
+sum(15,18);
+sum(18,12);
